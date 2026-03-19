0d5705303fbcf92d521afdbcf66243fdef1e9fa9
psvansluis <psvansluis@users.noreply.github.com>
2026-03-19 16:37:20 +0100

    more complex return type

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,12 +3,15 @@
   import { loadWasm } from './lib/wasm';
   let message = 'Loading...';
 
+  let spel = null;
   onMount(async () => {
     const wasm = await loadWasm();
     message = wasm.greet('Svelte + WASM');
+    spel = wasm.speel_boter_kaas_eieren();
   });
 </script>
 
 <main>
   <h1>{message}</h1>
+  <p>JS: {JSON.stringify(spel)}</p>
 </main>
--- rust-wasm/src/lib.rs
+++ rust-wasm/src/lib.rs
@@ -1,6 +1,22 @@
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct BoterKaasEieren {
+    bord: [[char; 3]; 3],
+    speler_met_beurt: char,
+}
 
 #[wasm_bindgen]
 pub fn greet(name: &str) -> String {
     format!("Hello, {} from Rust WASM!", name)
+}
+
+#[wasm_bindgen]
+pub fn speel_boter_kaas_eieren() -> JsValue {
+    let spel = BoterKaasEieren {
+        bord: [[' '; 3]; 3],
+        speler_met_beurt: 'X',
+    };
+    serde_wasm_bindgen::to_value(&spel).unwrap()
 }
